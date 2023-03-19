7171845513516e98b1ec4bb5c28ea84b489c33ad
mr-josh <mr-josh@sparklapse.com>
2023-03-19 23:25:29 +1100

    Plinko get API

--- func/PlinkoAPI/helpers/methods/get.py
+++ func/PlinkoAPI/helpers/methods/get.py
@@ -1,6 +1,29 @@
 from azure.functions import HttpRequest, HttpResponse
+from bson.objectid import ObjectId
+from bson.json_util import dumps
 from common.connections.db import dbc
+from common.connections.fs import get_container_client, generate_sas_url
+
+fsc = get_container_client("dots")
 
 
 async def plinko_get(req: HttpRequest):
-    return HttpResponse("Plinko GET request")
+    try:
+        last_seen = req.params.get("last", "000000000000000000000000")
+        last_seen = ObjectId(last_seen)
+    except:
+        return HttpResponse("Invalid last seen id", status_code=400)
+
+    latest = dbc["Dots"]["Plinko"].find_one(
+        {"_id": {"$gt": last_seen}},
+        sort=[("_id", -1)],
+    )
+
+    if not latest:
+        return HttpResponse("No new data", status_code=204)
+
+    blob = fsc.get_blob_client(latest["sim"])
+    url = generate_sas_url(blob)
+    latest["sim"] = url
+
+    return HttpResponse(dumps(latest), status_code=200)
--- func/common/connections/fs.py
+++ func/common/connections/fs.py
@@ -1,5 +1,25 @@
 import os
-from azure.storage.blob import ContainerClient
+from collections import namedtuple
+from datetime import datetime, timedelta
+from azure.storage.blob import (
+    ContainerClient,
+    BlobClient,
+    BlobSasPermissions,
+    generate_blob_sas,
+)
+
+AzureCredentials = namedtuple(
+    "Credentials",
+    ["DefaultEndpointsProtocol", "AccountName", "AccountKey", "EndpointSuffix"],
+)
+
+
+def get_credentials() -> AzureCredentials:
+    """Get credentials from connection string."""
+
+    return AzureCredentials(
+        *["=".join(c.split("=")[1:]) for c in os.environ["FS_CONNSTR"].split(";")]
+    )
 
 
 def get_container_client(container: str):
@@ -7,3 +27,21 @@ def get_container_client(container: str):
         conn_str=os.environ["FS_CONNSTR"],
         container_name=container,
     )
+
+
+def generate_sas_url(blob: BlobClient):
+    sas = generate_blob_sas(
+        account_name=str(blob.account_name),
+        container_name=blob.container_name,
+        blob_name=blob.blob_name,
+        account_key=get_credentials().AccountKey,
+        permission=BlobSasPermissions(read=True),
+        expiry=datetime.utcnow() + timedelta(minutes=10),
+    )
+
+    sas_url = (
+        f"https://{blob.account_name}.blob.core.windows.net/"
+        f"{blob.container_name}/{blob.blob_name}?{sas}"
+    )
+
+    return sas_url
